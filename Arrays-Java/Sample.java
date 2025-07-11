cfd43d98adcc40d2065a18ea9d95df3e118a8003
Devi Kontham <meghanakontham28@gmail.com>
2025-07-11 13:42:50 +0530

    Added sample.ajav under Arrays-Java

---
+++ Arrays-Java/Sample.java
@@ -0,0 +1,9 @@
+import java.util.*;
+public class Sample{
+public static void main(String[] args){
+    Scanner sc=new Scanner(System.in);
+    int[] a={1,2,3,4,5};
+    for(int i=0;i<5;i++){
+        System.out.print(a[i]+" ");
+    }
+}}
